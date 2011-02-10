a3994ad28d8b6a7108c56617aaa1a8d3a8e864b8
sweinstein <sweinstein@b387b25f-291c-0410-8dc7-e51caacd5b47>
2011-02-10 23:41:25 +0000

    moved to GitHub https://github.com/ScottWeinstein/Linq2KdbQ

--- contrib/sweinstein/linq2kdbplus/linq2kdb+/ToDo.cs
+++
@@ -1,8 +0,0 @@
-﻿//TODO Support kdb array types
-//TODO OrderBy
-//TODO GroupBy
-//TODO Aggregates
-//TODO Join
-//TODO Common SQL and Q functions
-//TODO Use comma func for & eval - ((=;`status;20);(=;`city;enlist `london))
-
